b154abd576f6445b5bd3ecc581ff6aa5b618a3a9
Kevin Ma <kbma.kevin@gmail.com>
2017-06-05 23:55:10 -0400

    added properties to expose the textboxes, button's texts, and group boxes' text property

--- KevinMaM17_Lab2_Ex2/SimpleRegistrationUserControl.cs
+++ KevinMaM17_Lab2_Ex2/SimpleRegistrationUserControl.cs
@@ -16,5 +16,49 @@ namespace KevinMaM17_Lab2_Ex2
         {
             InitializeComponent();
         }
+
+        //Public Properties to expose the text property of the constituent controls in this custom user control
+
+        //properties for group box title
+        public string GroupBoxTitle
+        {
+            get { return this.registrationGroupBox.Text; }
+            set { this.registrationGroupBox.Text = value; }
+        }
+
+        //properties for username
+        public string Username
+        {
+            get { return this.usrnameTB.Text; }
+            set { this.usrnameTB.Text = value; }
+        }
+
+        //properties for password
+        public string Password
+        {
+            get { return this.pwdTB.Text; }
+            set { this.pwdTB.Text = value; }
+        }
+
+        //properties for confirm password
+        public string ConfirmPassword
+        {
+            get { return this.confirmPwdTB.Text; }
+            set { this.confirmPwdTB.Text = value; }
+        }
+
+        //properties for submit btn
+        public string SubmitBtnText
+        {
+            get { return this.submitBtn.Text; }
+            set { this.submitBtn.Text = value; }
+        }
+
+        //properties for cancel btn
+        public string ClearBtnText
+        {
+            get { return this.clearBtn.Text; }
+            set { this.clearBtn.Text = value; }
+        }
     }
 }
